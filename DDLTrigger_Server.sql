3938f709f2a6d889f79e5572154701d70d282e63
LihiniK <lihinikarunarathne@gmail.com>
2023-07-17 22:33:55 +0300

    DDL Trigger on a SQL server to audit all the DDL server level events

---
+++ DDLTrigger_Server.sql
@@ -0,0 +1,65 @@
+USE [master]
+GO
+
+/****** Object:  DdlTrigger [DDLTrigger_Server]    Script Date: 6/29/2018 9:27:09 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+CREATE TRIGGER [DDLTrigger_Server]
+    ON ALL SERVER
+    FOR DDL_SERVER_LEVEL_EVENTS
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE
+        @EventData XML = EVENTDATA();
+ 
+    DECLARE 
+        @ip VARCHAR(32) =
+        (
+            SELECT client_net_address
+                FROM sys.dm_exec_connections
+                WHERE session_id = @@SPID
+        );
+ 
+    INSERT AuditDB.dbo.DDLEvents
+    (
+        EventType,
+        EventDDL,
+        EventXML,
+        DatabaseName,
+        SchemaName,
+        ObjectName,
+        HostName,
+        IPAddress,
+        ProgramName,
+        LoginName
+    )
+    SELECT
+        @EventData.value('(/EVENT_INSTANCE/EventType)[1]',   'NVARCHAR(100)'), 
+        @EventData.value('(/EVENT_INSTANCE/TSQLCommand)[1]', 'NVARCHAR(MAX)'),
+        @EventData,
+        DB_NAME(),
+        @EventData.value('(/EVENT_INSTANCE/SchemaName)[1]',  'NVARCHAR(255)'), 
+        @EventData.value('(/EVENT_INSTANCE/ObjectName)[1]',  'NVARCHAR(255)'),
+        HOST_NAME(),
+        @ip,
+        PROGRAM_NAME(),
+        SUSER_SNAME();
+END
+
+
+
+
+GO
+
+ENABLE TRIGGER [DDLTrigger_Server] ON ALL SERVER
+GO
+
+
